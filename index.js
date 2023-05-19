959d5a3bb6408cfbce003ee3389beaf0fda46418
KBradshaw64 <130187631+KBradshaw64@users.noreply.github.com>
2023-05-18 17:37:06 -0700

    added function to display current time and create local storage for inputs when hitting a save button

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+displayTime = function() {
+  $('#currentDay').text(dayjs().format('dddd MMM D, YYYY h:mm a'));
+}
+
+
+$(document).ready(function () {
+  displayTime()
+  setInterval(displayTime,1000)
+
+  $('.saveBtn').on("click",function(){
+    var text = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+    localStorage.setItem(time, text, JSON.stringify(time));
+    console.log(time, text);
+  });
+
+  
+
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
